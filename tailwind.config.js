aef3838c80d231a7ff652db9f6ea122b55076b16
CodeWithDheeru <147035446+CodeWithDheeru@users.noreply.github.com>
2026-01-05 18:45:05 +0530

    Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+     fontFamily:{
+      open:["Open Sans","serif"]
+     }
+    },
+  },
+  plugins: [],
+}
+
+
